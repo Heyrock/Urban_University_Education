c6d9a97addec6ddfb093d1722a50db29f342c042
Eugene Ilyinykh <napoluskr@yandex.ru>
2024-09-27 17:34:08 +0300

    Модуль_1_6

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-# list_ = [1, 2, 3, 4, 5, 6, 7, 8]
-# tuple_ = (1, 2, 3, 4, 5, 6, 7, 8)
-# print(list_.__sizeof__())  # 104
-# print(tuple_.__sizeof__())  # 88
-#
-# tuple_2 = ([1, 2, 3], 'a', 'b')
-# tuple_2[0].append(4)
-# print(tuple_2)  # ([1, 2, 3, 4], 'a', 'b')
-# tuple_2[0][1] = 200
-# print(tuple_2)  # ([1, 200, 3, 4], 'a', 'b')
-#
-
-tuple_ = (4, 'string')
-print(tuple_[1].replace('s', 'S'))  # String
-print(tuple_)  # (4, 'string')
-# ([1, 2, 3], 4, [1, 2, 3], 4, [1, 2, 3], 4)
-
+my_set = {1, 2, 3, 4, 5}
+my_set.add(6)
+print(my_set) # {1, 2, 3, 4, 5, 6}
--- xxx_testing.py
+++ xxx_testing.py
@@ -1,4 +1,5 @@
-
+my_set = {(1, 2, 3), 'a', 1}
+print(my_set)
 
 # if __name__ == '__main__':
 #     print(name, type(name))
