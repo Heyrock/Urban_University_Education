afde3b2c5f29126b0f4d3ffdf1a1d868509c5eb0
Eugene Ilyinykh <napoluskr@yandex.ru>
2024-10-01 17:25:53 +0300

    Модуль_2_2 Условия if, else, elif

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-print(dict([[1,2],[3,4]]))
-# {1: 2, 3: 4}
-print(dict([(3,26),(4,44)]))
-# {3: 26, 4: 44}
+# name = input('Введите ваше имя: ')
+# if name == 'xxx':
+#     print('Панки, хой!')
+# else:
+#     print(f'Дратути, {name}!')
+#
+number = int(input('Введите число: '))
+if number % 3 == 0 and number % 5 == 0:
+    print('FizzBuzz')
+elif number % 5 == 0:
+    print('Buzz')
+elif number % 3 == 0:
+    print('Fizz')
+else:
+    print('Wrong number')
