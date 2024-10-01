4a4d18eb2d14422eb1753eb54926d6f44a2bb358
Eugene Ilyinykh <napoluskr@yandex.ru>
2024-10-01 21:00:36 +0300

    Модуль_2_3 Цикл while

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-# name = input('Введите ваше имя: ')
-# if name == 'xxx':
-#     print('Панки, хой!')
-# else:
-#     print(f'Дратути, {name}!')
-#
-number = int(input('Введите число: '))
-if number % 3 == 0 and number % 5 == 0:
-    print('FizzBuzz')
-elif number % 5 == 0:
-    print('Buzz')
-elif number % 3 == 0:
-    print('Fizz')
-else:
-    print('Wrong number')
+while True:
+    number = int(input('Введите число: '))
+    if number % 2 == 0:
+        print('Число чётное')
+        continue
+    else:
+        print('Число нечётное')
+        # break
+    print('Меня не забыли')
+
+print('Я за циклом')
