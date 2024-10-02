1dc3a6e185ba4ef232db52886e1326574d64451c
Eugene Ilyinykh <napoluskr@yandex.ru>
2024-10-02 13:48:12 +0300

    Модуль_2_4 Цикл for

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-while True:
-    number = int(input('Введите число: '))
-    if number % 2 == 0:
-        print('Число чётное')
-        continue
-    else:
-        print('Число нечётное')
-        # break
-    print('Меня не забыли')
+for i in range(1, 5, 1):
+    for j in range(1, 5):
+        print(f'{i} x {j} = {j * i}', end='\t')
+    print()
 
-print('Я за циклом')
+# 1 x 1 = 1	1 x 2 = 2	1 x 3 = 3	1 x 4 = 4	
+# 2 x 1 = 2	2 x 2 = 4	2 x 3 = 6	2 x 4 = 8
+# 3 x 1 = 3	3 x 2 = 6	3 x 3 = 9	3 x 4 = 12
+# 4 x 1 = 4	4 x 2 = 8	4 x 3 = 12	4 x 4 = 16
