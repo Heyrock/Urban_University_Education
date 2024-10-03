d15fe8cb7704119c4d0699bde314b1efe7d3d102
Eugene <napoluskr@yandex.ru>
2024-10-03 14:30:44 +0300

    Модуль_2_5 Функции, начало

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-for i in range(1, 5, 1):
-    for j in range(1, 5):
-        print(f'{i} x {j} = {j * i}', end='\t')
-    print()
+current_list = [[10, 6, 9], [0, 14, 16, 80], [8, 12, 30, 44]]
+sorting_inner_lists = lambda cur_list: (sorted(i) for i in cur_list)
+# print(list(sorting_inner_lists(current_list)))
+# [[6, 9, 10], [0, 14, 16, 80], [8, 12, 30, 44]]
 
-# 1 x 1 = 1	1 x 2 = 2	1 x 3 = 3	1 x 4 = 4	
-# 2 x 1 = 2	2 x 2 = 4	2 x 3 = 6	2 x 4 = 8
-# 3 x 1 = 3	3 x 2 = 6	3 x 3 = 9	3 x 4 = 12
-# 4 x 1 = 4	4 x 2 = 8	4 x 3 = 12	4 x 4 = 16
+defining_second_largest = lambda cur_list, func: [y[-2] for y in func(cur_list)]
+result = defining_second_largest(current_list, sorting_inner_lists)
+print(result)
+
+# [9, 16, 30]
