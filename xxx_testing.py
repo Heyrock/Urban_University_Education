75026dd25d6b54c70d771548e4c9eab67afc04dc
Eugene <napoluskr@yandex.ru>
2024-10-04 15:46:53 +0300

    Модуль_2_hard Тяжелая задачка

--- xxx_testing.py
+++ xxx_testing.py
@@ -1,5 +1,27 @@
-my_set = {(1, 2, 3), 'a', 1}
-print(my_set)
+# num_9 = sum(int(i) for i in '1218273645')
+# num_10 = sum(int(i) for i in '141923283746')
+# num_11 = sum(int(i) for i in '11029384756')
+# num_15 = sum(int(i) for i in '1214114232133124115106978')
+# num_20 = sum(int(i) for i in '13141911923282183731746416515614713812911')
+num_ref = sum(int(i) for i in '13141911923282183731746416515614713812911')
+num_test = sum(int(i) for i in '13142319283746119218317416515614713812911')
 
-# if __name__ == '__main__':
-#     print(name, type(name))
+# print(num_9) # 39
+# print(num_10) # 50
+# print(num_11) # 46
+# print(num_15) # 73
+# print(num_20) # 153
+
+print(num_ref)
+print(num_test)
+
+num_1 = 9
+summands = []
+
+
+for j in range(1, (num_1 + 1) // 2):
+    # print(j)
+    summands.append((j, num_1 - j))
+
+
+print(summands)
