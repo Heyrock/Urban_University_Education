9986f6dcee9a9b6e0ad00944324d6e9ee12be4ec
Eugene <napoluskr@yandex.ru>
2024-10-09 15:18:15 +0300

    Модуль_3_2 - задача про адреса писем

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
-# Задание 4
-# Напишите программу, которая получает от пользователя строку текста
-# и число n, а затем выводит вложенный список, в котором n последовательных
-# элементов принадлежат разным подспискам.
-string = 'абвгдеёжз'
-num = 3
-matrix = []
-count = 0
+def func_with_params(a, b=None):
+    if b is None:
+        b = []
+        b.append(a)
+    print(b)
 
-for i in range(num):
-    matrix.append([])
-    for j in range(count, len(string), num):
-        matrix[i].append(string[j])
-    count += 1
 
-[print(*matrix[i]) for i in range(len(matrix))]
+[func_with_params(3) for i in range(3)]
 
-# ИЛИ:
-
-string = list(input())
-num = int(input())
-matrix = []
-
-for i in range(num):
-    matrix.append(string[i::num])
-print(matrix)
+# [3]
+# [3, 3]
+# [3, 3, 3]
--- xxx_testing.py
+++ xxx_testing.py
@@ -1,22 +1,21 @@
-# 1+3 1+4 1+9 1+19 2+3 2+8 2+18 3+7 3+17 4+6 4+16 5+15 6+14 7+13 8+12 9+11
-# 13141911923282183731746416515614713812911
+def test(x, y):
+    if check(x, y):
+        print('Ok')
+    elif check_2(x, y):
+        print('X is larger')
+    elif check_3(x, y):
+        print('Y is larger')
+    else:
+        print('Whatever')
 
-nums = 20
-my_list = [i for i in range(3, nums + 1) if nums % i == 0]
-print(f'my_list = {my_list}')
-new_list = []
-
-for i in range(1, nums // 2):
-    for num in my_list:
-        if i < (num + 1)// 2:
-            print(f'{i}+{num - i}')
-            new_list.append(i)
-            new_list.append(num - i)
-    print()
+def check(x, y):
+    return x == y
 
 
-print(''.join(map(str, new_list)))
-print('13141911923282183731746416515614713812911')
+def check_2(x, y):
+    return x > y
 
-# 1314191192  82183731746416515614713812911
-# 13141911923282183731746416515614713812911
+def check_3(x, y):
+    return x < y
+
+test(0, 0)
