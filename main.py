4e768109d2560633013e0beed49ce7f523023dbd
Eugene <napoluskr@yandex.ru>
2024-10-25 12:09:58 +0300

    module_4_2 - области видимости

--- main.py
+++ main.py
@@ -1,41 +1,104 @@
-# Задание 6
-# В предыдущих статьях мы неоднократно использовали встроенную функцию zip()
-# для параллельной итерации двух наборов данных.
-
-# Напишите функцию высшего порядка, которая возвращает функции для:
+# Задание 10
+# Напишите декоратор, который будет измерять производительность функций,
+# создающих список с помощью этих методов:
 #
-# 1. Группировки параллельных элементов списков с помощью самописной my_zip().
-# 2. Конкатенации элементов, сгрупированных my_zip().
-# 3. Сложения элементов, сгруппированных my_zip().
+# range()
+# списковое включение
+# append()
+# конкатенация
+# Среди показателей должны быть:
+#
+# Время работы функции.
+# Текущее потребление памяти.
+# Пиковое потребление памяти.
+#
 
-# Примечание: следует учесть, что получаемые от пользователя списки могут
-# быть разной длины.
-# Как и встроенная zip(), my_zip() должна ограничивать размер возвращаемого
-# списка длиной более короткого набора данных.
-
-# (5, 4) (8, 5) (9, 6) (8, 2) (3, 3) (12, 2) (3, 5) (5, 9) (5, 4) (4, 12) (0, 9) (9, 3)
-# 54 85 96 82 33 122 35 59 54 412 09 93
-# 9 13 15 10 6 14 8 14 9 16 9 12
-def convert(*args):
-    print(args)
-    return (list(map(int, arg.split())) for arg in args)
-
-
-def zipp(*args):
-    lst_1, lst_2 = convert(args)
-    length = min(len(lst_1), len(lst_2))
-    new_list = []
-    for i in range(length):
-        new_list.append((lst_1[i], lst_2[i]))
+import tracemalloc
+from time import perf_counter
+from functools import wraps
+import inspect
 
 
-def my_zip(f):
-    def medium(*args):
-        return f(args)
-    return medium
+def time_memory_used(function):
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        tracemalloc.start()
+        start = perf_counter()
+        result = function(*args, **kwargs)
+        current, peak = tracemalloc.get_traced_memory()
+        stop = perf_counter()
+        print(f'Название функции: {function.__name__}')
+        print(f'Использованный метод: {function.__doc__}')
+        print(f'Текущее потребление памяти: {current / 10**6:.6f} мб \n'
+              f'Пик использования памяти: {peak / 10**6:.6f} мб ')
+        print(f'Операция заняла: {stop - start:.6f} секунд')
+        tracemalloc.stop()
+        return result
+    return wrapper
 
-input_1 = '5 8 9 8 3 12 3 5 5 4 0 9 6 1 23 6 12 30'
-input_2 = '4 5 6 2 3 2 5 9 4 12 9 3'
 
-x = my_zip(zipp)
-print(x(input_1, input_2))
+@time_memory_used
+def make_list_with_range():
+    'range()'
+    my_list = list(range(100000))
+    # print(inspect.stack()) # Интересненько
+    return f'Функция {inspect.stack()[0][3]} завершила работу \n{"-" * 48}'
+
+@time_memory_used
+def make_list_comprehension():
+    'list comprehension'
+    my_list = [l for l in range(100000)]
+    return f'Функция {inspect.stack()[0][3]} завершила работу \n{"-" * 48}'
+
+
+@time_memory_used
+def make_list_with_append():
+    'append()'
+    my_list = []
+    for item in range(100000):
+        my_list.append(item)
+    return f'Функция {inspect.stack()[0][3]} завершила работу \n{"-" * 48}'
+
+
+@time_memory_used
+def make_list_concatenation():
+    'конкатенация'
+    my_list = []
+    for item in range(100000):
+        my_list = my_list + [item]
+    return f'Функция {inspect.stack()[0][3]} завершила работу \n{"-" * 48}'
+
+
+# print(make_list_with_range())
+# print(make_list_comprehension())
+# print(make_list_with_append())
+# print(make_list_concatenation())
+#
+# Название функции: make_list_with_range
+# Использованный метод: range()
+# Текущее потребление памяти: 0.034127 мб
+# Пик использования памяти: 4.038928 мб
+# Операция заняла: 0.092711 секунд
+# Функция make_list_with_range завершила работу
+# ------------------------------------------------
+# Название функции: make_list_comprehension
+# Использованный метод: list comprehension
+# Текущее потребление памяти: 0.000526 мб
+# Пик использования памяти: 3.994862 мб
+# Операция заняла: 0.067124 секунд
+# Функция make_list_comprehension завершила работу
+# ------------------------------------------------
+# Название функции: make_list_with_append
+# Использованный метод: append()
+# Текущее потребление памяти: 0.000522 мб
+# Пик использования памяти: 3.994870 мб
+# Операция заняла: 0.072619 секунд
+# Функция make_list_with_append завершила работу
+# ------------------------------------------------
+# Название функции: make_list_concatenation
+# Использованный метод: конкатенация
+# Текущее потребление памяти: 0.000526 мб
+# Пик использования памяти: 4.791808 мб
+# Операция заняла: 21.387560 секунд
+# Функция make_list_concatenation завершила работу
+# ------------------------------------------------
