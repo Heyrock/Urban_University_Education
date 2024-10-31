cb79e74be17e00eb1fd4003111477a4830147883
Eugene <napoluskr@yandex.ru>
2024-10-31 12:35:34 +0300

    module_5_2 - Классы. Спецметоды

--- main.py
+++ main.py
@@ -9,9 +9,24 @@ class Human:
 
     def birthday(self):
         self.age += 1
-        print(f'У меня сегодня день рождения, мне исполнилось {self.age}')
+        print(f'У {self.name} сегодня день рождения, исполнилось {self.age}')
+
+    def __str__(self):
+        return self.name
+
+    def __len__(self):
+        return self.age
+
+    def __del__(self):
+        print(f'{self.name} ушел')
 
 
-eugen = Human(name='Евгений', age=20)
+eugen = Human('Евгений', 20)
 max = Human(name='Максим', age=18)
-
+eugen.birthday()
+max.birthday()
+print(eugen.__len__())
+print(len(eugen))
+print(eugen)
+# del eugen
+# input()
--- module_5_1.py
+++ module_5_1.py
@@ -45,8 +45,11 @@
 
 class House:
     def __init__(self, name, number_of_floors):
-        self.name = name,
+        self.name = name
         self.number_of_floors = number_of_floors
+
+    def __str__(self):
+        return self.name
 
     def go_to(self, new_floor):
         if new_floor <= self.number_of_floors:
