5182b01d66e4b275c7137f50577ec6fef818b29f
Eugene <napoluskr@yandex.ru>
2024-12-16 16:17:16 +0300

    module_10_4 - задача про потоки гостей в кафе

--- Homework/temp.py
+++ Homework/temp.py
@@ -1,5 +1,15 @@
-x = None
-if x:
-    print('Yes')
-else:
-    print('No')
+import operator
+
+a = [1, 2, 3, 4, 5]
+b = ['a', 'b', 'c', 'd', 'e']
+x = 3
+
+print(operator.concat(a, b))
+# [1, 2, 3, 4, 5, 'a', 'b', 'c', 'd', 'e']
+print(operator.contains(a, x))
+# True
+print(operator.countOf(a, x))
+# 1
+
+
+# Если очередь ещё не пуста (метод empty) и один из столов освободился
