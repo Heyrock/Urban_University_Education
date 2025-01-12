9a1ddd983fd149b8dfe1ef990c1a8af7f9887521
Eugene <napoluskr@yandex.ru>
2025-01-12 17:08:54 +0300

    module_11_4 - Доработка. Интроспекция

--- Homework/module_11_4.py
+++ Homework/module_11_4.py
@@ -1,3 +1,34 @@
+# Закрепить знания об интроспекции в Python.
+# Создать персональную функции для подробной интроспекции объекта.
+#
+# Задание:
+# Необходимо создать функцию, которая принимает объект (любого типа)
+# в качестве аргумента и проводит интроспекцию этого объекта,
+# чтобы определить его тип, атрибуты, методы, модуль, и другие свойства.
+#
+# 1. Создайте функцию introspection_info(obj), которая принимает объект obj.
+# 2. Используйте встроенные функции и методы интроспекции Python для получения
+# информации о переданном объекте.
+# 3. Верните словарь или строки с данными об объекте, включающий следующую
+# информацию:
+# - Тип объекта.
+# - Атрибуты объекта.
+# - Методы объекта.
+# - Модуль, к которому объект принадлежит.
+# - Другие интересные свойства объекта, учитывая его тип (по желанию).
+#
+# Пример работы:
+# number_info = introspection_info(42)
+# print(number_info)
+#
+# Вывод на консоль:
+# {'type': 'int', 'attributes': [...], 'methods': ['__abs__', '__add__', ...],
+# 'module': '__main__'}
+#
+# Рекомендуется создавать свой класс и объект для лучшего понимания
+# Файл с кодом прикрепите к домашнему заданию.
+
+import inspect
 from pprint import pprint
 import re
 
@@ -22,12 +53,16 @@ def get_methods(obj):
     return [i for i in dir(obj) if i not in get_attr(obj) and '__' not in i]
 
 
+def get_module(obj):
+    return inspect.getmodule(obj)
+
+
 def introspection_info(obj):
     results = {}
     results['type'] = get_type(obj)
     results['attributes'] = get_attr(obj)
     results['methods'] = get_methods(obj)
-    # results['module'] =
+    results['module'] = get_module(obj)
     return results
 
 
@@ -42,13 +77,7 @@ class TestClass:
 
 instance = TestClass()
 instance_info = introspection_info(instance)
-print(instance_info)
-print(dir(instance))
+pprint(instance_info)
+
 number_info = introspection_info(42)
-print(number_info)
-print(dir(42))
-
-
-
-# {'type': 'int', 'attributes': [...], 'methods': ['__abs__', '__add__', ...],
-# 'module': '__main__'}
+pprint(number_info)
